08e44b39f929e782dc5d671fd957badf1b979cfd
jaumard <jimmy.aumard@gmail.com>
2019-10-08 07:50:29 +0200

    fix wrong provider

--- packages/app/lib/main.dart
+++ packages/app/lib/main.dart
@@ -121,20 +121,17 @@ class MyHomePage extends HookWidget {
   Widget build(BuildContext context) {
     final store = useMemoized(() => SpeechStore());
     final userStore = Provider.of<UserStore>(context);
-    return ListenableProvider.value(
-      value: Router(),
-      child: ProxyScaffold(
-        floatingActionButton: kIsMobile()
-            ? SpeechButton(
-                onResults: (text) async {
-                  final response = await store.sendSentence(text, Localizations.localeOf(context).languageCode);
-                  Toast.show(response, context, duration: Toast.LENGTH_LONG);
-                },
-              )
-            : null,
-        builderDrawer: (context) => AppDrawer(),
-        initialRoute: FavoritesWidget.route, //FIXME why it doesn't work?? check routes.dart userStore.lastRoute ?? FavoritesWidget.route,
-      ),
+    return ProxyScaffold(
+      floatingActionButton: kIsMobile()
+          ? SpeechButton(
+              onResults: (text) async {
+                final response = await store.sendSentence(text, Localizations.localeOf(context).languageCode);
+                Toast.show(response, context, duration: Toast.LENGTH_LONG);
+              },
+            )
+          : null,
+      builderDrawer: (context) => AppDrawer(),
+      initialRoute: FavoritesWidget.route, //FIXME why it doesn't work?? check routes.dart userStore.lastRoute ?? FavoritesWidget.route,
     );
   }
 }
