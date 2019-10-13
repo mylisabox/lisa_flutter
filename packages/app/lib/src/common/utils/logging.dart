ec6f92224a8cf2fd769186014d90052a3cd563e8
jaumard <jimmy.aumard@gmail.com>
2019-10-13 13:45:58 +0200

    add sentry crash reporting

--- packages/app/lib/src/common/utils/logging.dart
+++ packages/app/lib/src/common/utils/logging.dart
@@ -1,26 +1,66 @@
+import 'dart:collection';
 import 'dart:developer';
 
 import 'package:flutter/widgets.dart';
-import 'package:flutter_crashlytics/flutter_crashlytics.dart';
+import 'package:intl/intl.dart';
 import 'package:lisa_flutter/src/common/constants.dart';
 import 'package:logging/logging.dart';
+import 'package:sentry/sentry.dart';
+
+final SentryClient sentry = SentryClient(dsn: 'https://53c5686c50434e9884c1a763b984af58@sentry.io/1777712');
 
 const _logFilter = [];
+const _memLogMaxLines = 500;
+final _memLogDateFormatter = DateFormat('yyyy-MM-dd HH:mm:ss.SSS');
+final _logs = Queue<String>();
 
 void initLogger() {
   Logger.root.level = Level.ALL;
   Logger.root.onRecord.listen(_log);
 }
 
+
+void _memLog(LogRecord record) {
+
+  // only keep last n lines
+  if (_logs.length >= _memLogMaxLines) {
+    _logs.removeFirst();
+  }
+
+  var level = '?';
+  switch (record.level.value) {
+    case 300: level = 'T'; break;
+    case 400:
+    case 500: level = 'D'; break;
+    case 800: level = 'I'; break;
+    case 900: level = 'W'; break;
+    case 1000: level = 'E'; break;
+    case 1200: level = 'WTF'; break;
+  }
+
+  _logs.add('${_memLogDateFormatter.format(DateTime.now())} $level/${record.loggerName} ${record.message}');
+}
+
+
 void _log(LogRecord record) async {
   // log to crashlytics
   if (kIsProductionMode) {
-    await FlutterCrashlytics().log(record.message, priority: record.level.value, tag: record.loggerName);
+    _memLog(record);
   }
 
   // send as crash to crashlytics if severe
   if (kIsProductionMode && record.level >= Level.SEVERE && record.stackTrace != null) {
-    await FlutterCrashlytics().reportCrash(record.error, record.stackTrace, forceCrash: false);
+    await sentry.capture(
+      event: Event(
+        //TODO release https://github.com/dart-lang/sdk/issues/38855
+        extra: {'log': _logs.toList(growable: false)},
+        culprit: record.error.toString(),
+        loggerName: record.loggerName,
+        exception: record.error,
+        stackTrace: record.stackTrace,
+      ),
+    );
+    _logs.clear();
   } else if (!kIsProductionMode) {
     // now, log!
     if (record.stackTrace != null) {
